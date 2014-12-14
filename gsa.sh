07bbe3c727af91b0bcb9b400adaa1f276545bd7e
Clive C <doobahead@gmail.com>
2014-12-14 01:04:42 -0500

    Further improved

--- gsa.sh
+++ gsa.sh
@@ -1,3 +1,9 @@
+#
+# Notes:
+# if it's not "Up-to-date and nothing to commit.", that means you're either not up to date or you haven't --set-upstream-to= yet.
+# if it says "fatal: no remote repository specified" it means that you don't have a remote set up or haven't set it to upstream yet.
+#
+
 find -type d -name .git -prune -exec bash -c '
 cd "{}/..";
 echo "
@@ -5,15 +11,8 @@ echo "
 --------{}--------";
 git fetch;
 git branch -a -vv;
-stat=$(git status);
+stat=$(git -c color.status=always status);
 if grep -lq "nothing to commit, working directory clean" <<<"$stat" >nul && grep -lq "Your branch is up-to-date with " <<<"$stat" >nul;
-then echo "Up-to-date and nothing to commit.";
+then echo "Up-to-date and nothing to commit (on current branch).";
 else echo "$stat";
 fi' \;
-
-#
-# Notes:
-# if it's not "Up-to-date and nothing to commit.", that means you're either not up to date or you haven't --set-upstream-to= yet.
-# if it says "fatal: no remote repository specified" it means that you don't have a remote set up or haven't set it to upstream yet.
-#
-
