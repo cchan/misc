93a103cd848c4119d47f73f5d528a0dae9d670b0
Clive C <doobahead@gmail.com>
2014-12-14 00:52:37 -0500

    Added verbosity to gitallstatus

--- gsa.sh
+++ gsa.sh
@@ -4,9 +4,16 @@ echo "
 
 --------{}--------";
 git fetch;
-git branch -a;
+git branch -a -vv;
 stat=$(git status);
 if grep -lq "nothing to commit, working directory clean" <<<"$stat" >nul && grep -lq "Your branch is up-to-date with " <<<"$stat" >nul;
 then echo "Up-to-date and nothing to commit.";
 else echo "$stat";
 fi' \;
+
+#
+# Notes:
+# if it's not "Up-to-date and nothing to commit.", that means you're either not up to date or you haven't --set-upstream-to= yet.
+# if it says "fatal: no remote repository specified" it means that you don't have a remote set up or haven't set it to upstream yet.
+#
+
