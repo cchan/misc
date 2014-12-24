b27ece9dc4b871747bc7be3da16aca16fb0fc071
Clive C <doobahead@gmail.com>
2014-12-23 20:26:48 -0500

    Made GSA really awesome

--- gitshell/gsa.sh
+++ gitshell/gsa.sh
@@ -7,12 +7,32 @@
 find -type d -name .git -prune -exec bash -c '
 cd "{}/..";
 echo "
-
 --------{}--------";
-git fetch;
-git branch -a -vv;
-stat=$(git -c color.status=always status);
-if grep -lq "nothing to commit, working directory clean" <<<"$stat" >nul && grep -lq "Your branch is up-to-date with " <<<"$stat" >nul;
-then echo "Up-to-date and nothing to commit (on current branch).";
-else echo "$stat";
-fi' \;
+git remote update >/dev/null
+git -c color.status=always status -s
+
+for branch in $(git for-each-ref --sort="-authordate:iso8601" --format="%(refname)" refs/heads/); do
+    SHORT=$(basename "$branch")
+	
+    echo -e -n "\E[1;33m$SHORT:\E[0m "
+    if [[ -n $(git config --get branch.$SHORT.remote) ]]; then
+        LOCAL=$(git rev-parse "$SHORT")
+        REMOTE=$(git rev-parse "$SHORT"@{upstream})
+        BASE=$(git merge-base "$SHORT" "$SHORT"@{upstream})
+        
+        if [ $LOCAL = $REMOTE ]; then
+            echo -e "\E[32mUp-to-date.\E[0m"
+        elif [ $LOCAL = $BASE ]; then
+            echo -e "\E[31mNeed to pull!\E[0m"
+        elif [ $REMOTE = $BASE ]; then
+            echo -e "\E[31mNeed to push!\E[0m"
+        else
+            echo -e "\E[31mDiverged!!\E[0m"
+        fi
+    else
+        echo "No upstream configured."
+    fi
+    git --no-pager log --pretty=format:"LOCAL: %ar	%<(50,trunc)%s" "$SHORT" -n 1
+    echo.
+done
+' \;
