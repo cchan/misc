a4e783b659ad2ed579c2a28db52f1f206a3fa143
Clive Chan <cc@clive.io>
2016-08-03 00:30:39 -0400

    ayeee promsongs script

---
+++ promsongs.sh
@@ -0,0 +1,31 @@
+# Progress: http://platinumproductionsma.djintelligence.com/request/print.asp?ID=06-02-16
+# https://gyazo.com/42933fef1568563a997bfcc2290f5fda
+
+for ((n=0;n<352;n++))
+do
+
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Astley%2C+Rick&SONG=Never+Gonna+Give+You+Up&OPTION1=1980%27s&artistsearch=&songsearch=Never+Gonna+Give+You+Up&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'rick astley' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Menzel%2C+Idina&SONG=Let+It+Go&OPTION1=2010%27s&artistsearch=&songsearch=Let+It+Go&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'let it go' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Darude&SONG=Sandstorm&OPTION1=2000%27s&artistsearch=&songsearch=Sandstorm&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'darude sandstorm' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Martin%2C+Dean&SONG=That%27s+Amore&OPTION1=1950%27s&artistsearch=&songsearch=That%27s+Amore&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'amore' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Swift%2C+Taylor&SONG=I+Knew+You+Were+Trouble&OPTION1=2010%27s&artistsearch=&songsearch=I+Knew+You+Were+Trouble&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'tswift' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Beach+Boys&SONG=God+Only+Knows&OPTION1=1960%27s&artistsearch=&songsearch=God+Only+Knows&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'beachboys' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Goo+Goo+Dolls&SONG=Take+Me+Out+To+The+Ball+Game&OPTION1=1990%27s&artistsearch=&songsearch=Take+Me+Out+To+The+Ball+Game&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'ballgame' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Shakira&SONG=Waka+Waka+%28This+Time+For+Africa%29&OPTION1=2010%27s&artistsearch=&songsearch=Waka+Waka+%28This+Time+For+Africa%29&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'shakira' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Carey%2C+Mariah&SONG=All+I+Want+For+Christmas+Is+You&OPTION1=1990%27s&artistsearch=&songsearch=All+I+Want+For+Christmas+Is+You&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'christmas' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Koz%2C+Dave&SONG=%27Twas+The+Night+Before+Christmas&OPTION1=2000%27s&artistsearch=&songsearch=%27Twas+The+Night+Before+Christmas&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'christmas2' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Survivor&SONG=Eye+Of+The+Tiger&OPTION1=1980%27s&artistsearch=&songsearch=Eye+Of+The+Tiger&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'eye of the tiger' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Guetta%2C+David+Feat.+Sia&SONG=Titanium&OPTION1=2010%27s&artistsearch=&songsearch=titanium&artistbrowse=&songbrowse=&sort=artist&start=1&ID=06-02-16#results" -s >/dev/null && echo 'titanium' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Minaj%2C+Nicki&SONG=Starships&OPTION1=2010%27s&artistsearch=&songsearch=starships&artistbrowse=&songbrowse=&sort=artist&start=1&ID=06-02-16#results" -s >/dev/null && echo 'starships' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Legend%2C+John&SONG=All+Of+Me&OPTION1=2010%27s&artistsearch=legend&songsearch=all+of+me&artistbrowse=&songbrowse=&sort=artist&start=1&ID=06-02-16#results" -s >/dev/null && echo 'all of me' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Imagine+Dragons&SONG=Demons&OPTION1=2010%27s&artistsearch=&songsearch=demons&artistbrowse=&songbrowse=&sort=artist&start=1&ID=06-02-16#results" -s >/dev/null && echo 'demons' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=OneRepublic&SONG=Counting+Stars&OPTION1=2010%27s&artistsearch=&songsearch=counting+stars&artistbrowse=&songbrowse=&sort=artist&start=1&ID=06-02-16#results" -s >/dev/null && echo 'counting stars' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Ylvis&SONG=The+Fox&OPTION1=2010%27s&artistsearch=ylvis&songsearch=&artistbrowse=&songbrowse=&sort=artist&start=1&ID=06-02-16#results" -s >/dev/null && echo 'what does the fox say' &
+#curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Ronson%2C+Mark+Feat.+Bruno+Mars&SONG=Uptown+Funk&OPTION1=2010%27s&artistsearch=&songsearch=Uptown+Funk&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-02-16#results" -s >/dev/null && echo 'uptown funk' &
+curl "http://platinumproductionsma.djintelligence.com/request/request.asp?ACTION=add&ARTIST=Astley%2C+Rick&SONG=Never+Gonna+Give+You+Up&OPTION1=1980%27s&artistsearch=&songsearch=Never+Gonna+Give+You+Up&artistbrowse=&songbrowse=&sort=song&start=1&ID=06-22-16#results" -s >/dev/null && echo 'rick astley' &
+#curl "" -s >/dev/null && echo '' &
+
+done
+
+echo 'waiting'
+wait
