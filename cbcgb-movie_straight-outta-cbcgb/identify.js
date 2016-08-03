f18cd47798e6773fd63f63ff4ec0ada67f44ccb1
Clive Chan <cc@clive.io>
2016-08-02 23:44:33 -0400

    add CBCGB movie scripts - to simulate 'hacking' and some sort of people database lookup

---
+++ cbcgb-movie_straight-outta-cbcgb/identify.js
@@ -0,0 +1,13 @@
+console.log("Searching for "+process.argv[2]+" "+process.argv[3]);
+setTimeout(function(){
+	console.log("1 Record(s) found for search: ")
+	console.log("Name: Solomon Wang");
+	console.log("Street name: SlimSolo");
+	console.log("Last Known Address (address): 42 Wallaby Way");
+	console.log("Last Known Address (city): Lexington, MA");
+	console.log("Age: 69");
+	console.log("Criminal record: 72 total records");
+	console.log("Criminal record: 54 total felonies");
+	console.log("Prison term: 90 years (no parole)");
+	console.log("Escaped: 12/3/2013");
+},1000);
