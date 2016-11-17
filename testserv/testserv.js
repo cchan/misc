26df960f47a7b3ebffd0f4a6d323b26582e532c2
Clive Chan <cc@clive.io>
2016-11-17 00:45:56 -0500

    footer

--- testserv/testserv.js
+++ testserv/testserv.js
@@ -7,7 +7,17 @@ require('http').createServer((req,res)=>res.end(`
     padding-top:4em;
     color:#462201;
   }
+  footer{
+    position:fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+    opacity: 0.2;
+    padding-bottom: 2em;
+  }
 </style>
 <h1>i'm a little server</h1>
 <img src='http://cdn0.dailydot.com/uploaded/images/original/2014/8/18/pusheen.gif'>
+<footer><a href="https://clive.io">clive</a>'s test server</footer>
 `)).listen(8848,()=>console.log('listen 8848'));
