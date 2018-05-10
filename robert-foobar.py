71bbc8843d0b4f0b5aefb5f0736156d672cbd19c
Clive Chan <cc@clive.io>
2018-05-10 00:54:53 -0400

    LOL WHAT ONE LINE

--- robert-foobar.py
+++ robert-foobar.py
@@ -5,8 +5,10 @@
     # if n in lastSeen:
       # return i - lastSeen[n], lastSeen
 
-def solution(n, b, lastSeen = {}, i = 0):
-  return (i - lastSeen[n], lastSeen) if n in lastSeen else (solution(''.join(sorted(str((int(''.join(sorted(n))[::-1], b) - int(''.join(sorted(n)), b))//b**j%b) for j in range(len(n)))), b, dict(list(lastSeen.items()) + [(''.join(sorted(n)), i)]), i + 1))
+#def solution(n, b, lastSeen = {}, i = 0):
+#  return (i - lastSeen[n], lastSeen) if n in lastSeen else (solution(''.join(sorted(str((int(''.join(sorted(n))[::-1], b) - int(''.join(sorted(n)), b))//b**j%b) for j in range(len(n)))), b, dict(list(lastSeen.items()) + [(''.join(sorted(n)), i)]), i + 1))
+
+solution = lambda n, b, lastSeen = {}, i = 0: (i - lastSeen[n], lastSeen) if n in lastSeen else (solution(''.join(sorted(str((int(''.join(sorted(n))[::-1], b) - int(''.join(sorted(n)), b))//b**j%b) for j in range(len(n)))), b, dict(list(lastSeen.items()) + [(''.join(sorted(n)), i)]), i + 1))
 
 print(solution("210022", 3))
 print(solution("1211", 10))
