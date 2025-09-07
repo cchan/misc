fabcb569e1323fa3ceac2aee87b63b80a9c0c8d9
Clive Chan <cc@clive.io>
2025-09-07 17:12:28 +0000

    echo server

--- testserv/testserv.c
+++ testserv/testserv.c
@@ -4,12 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <fcntl.h>
 
 // todo next: remove the sleep(), add pthread, better error & TCP-signal handling,
 //    generalize this to express-like simplicity of routes
+
+// https://techtake.info/2016/09/19/non-blocking-tcp-server-explained/
+// https://beej.us/guide/bgnet/html/#blocking
 
 void error(const char *msg)
 {
@@ -21,25 +26,24 @@ int main(int argc, char *argv[])
 {
   int sockfd, newsockfd, portno, webpagefd;
   socklen_t clilen;
-  char buffer[256],
-    webpage[] = "HTTP/1.1 200 OK\r\nContent-Length: 344\r\n\r\n<style>body{background-color:#FCF0E4;font-family:Arial;text-align:center;padding-top:4em;color:#462201;}footer{position:fixed;bottom:0;left:0;right:0;text-align:center;opacity:0.2;padding-bottom:2em;}</style><h1>i'm a little server</h1><img src='https://clive.io/testserv.gif'><footer><a href='https://clive.io'>clive</a>'s test server</footer>",
-    errpage[] = "HTTP/1.1 404 Not Found\r\nContent-Length: 521\r\n\r\n<style>html,body{height:100%;background-color:#FCF0E4;font-family:Arial,sans-serif;text-align:center;color:#462201;font-size:1.1em;margin:0;}img{height:60%;}a{font-weight:bold;color:#8c4402}footer{position:absolute;font-size:0.7em;bottom:0.5em;width:100%;text-align:center;opacity:0.5;}</style><h1><img src='https://clive.io/img/404.gif' alt='glitched.'/></h1><p><b>404 Not Found!</b> This isn't the cat you're looking for.</p><p><a href='/'>Go home?</a></p><footer><a href='https://clive.io'>clive</a>'s website</footer>";
+  char buffer[256];
+  bzero(buffer, 256);
+  const char webpage[] = "HTTP/1.1 200 OK\r\nContent-Length: 344\r\n\r\n<style>body{background-color:#FCF0E4;font-family:Arial;text-align:center;padding-top:4em;color:#462201;}footer{position:fixed;bottom:0;left:0;right:0;text-align:center;opacity:0.2;padding-bottom:2em;}</style><h1>i'm a little server</h1><img src='https://clive.io/testserv.gif'><footer><a href='https://clive.io'>clive</a>'s test server</footer>";
+  const char errpage[] = "HTTP/1.1 404 Not Found\r\nContent-Length: 521\r\n\r\n<style>html,body{height:100%;background-color:#FCF0E4;font-family:Arial,sans-serif;text-align:center;color:#462201;font-size:1.1em;margin:0;}img{height:60%;}a{font-weight:bold;color:#8c4402}footer{position:absolute;font-size:0.7em;bottom:0.5em;width:100%;text-align:center;opacity:0.5;}</style><h1><img src='https://clive.io/img/404.gif' alt='glitched.'/></h1><p><b>404 Not Found!</b> This isn't the cat you're looking for.</p><p><a href='/'>Go home?</a></p><footer><a href='https://clive.io'>clive</a>'s website</footer>";
   int webpagelen = strlen(webpage), errpagelen = strlen(errpage);
   struct sockaddr_in serv_addr, cli_addr;
   char *PORT_str;
-  
-  //printf("%s\n", PORT_str);
-  
-  printf("%s\n", getenv("PORT"));
-  
+
   if((PORT_str = getenv("PORT")) == NULL)
     error("no $PORT provided");
   if ((portno = atoi(PORT_str)) <= 0 || portno > 65535)
     error("invalid port");
-  
+
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     error("opening socket");
-  
+  if (fcntl(sockfd, F_SETFL, O_NONBLOCK) == -1)
+    error("setting nonblock");
+
   bzero((char *) &serv_addr, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(portno);
@@ -47,37 +51,43 @@ int main(int argc, char *argv[])
   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
     error("binding");
   listen(sockfd, 5);
-  
-  //bzero(webpage, 512);
-  //if ((webpagefd = open("httpresponse.txt")) < 0)
-  //  error("getting webpage file");
-  //read(webpagefd, webpage, 511);
-  
+
   printf("Listening on port %d\n", portno);
-  
+
   while(1){
     clilen = sizeof(cli_addr);
-    if ((newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) < 0) 
-      error("on accept");
-    
-    /*
-    if (write(newsockfd, webpage, webpagelen) < 0)
-      error("writing to socket");
-    */
-    bzero(buffer, 256);
-    if (read(newsockfd, buffer, 255) < 0)
-      error("reading from socket");
+    if ((newsockfd = accept4(sockfd, (struct sockaddr *) &cli_addr, &clilen, SOCK_NONBLOCK | SOCK_CLOEXEC)) < 0)
+      continue; //error("on accept");
+
+    //int yes = 1;
+    //if (setsockopt(newsockfd, IPPROTO_TCP, TCP_NODELAY, &yes, sizeof(yes)) == -1) {
+    //  close(newsockfd);
+    //  continue;
+    //  // error("setsockopt error");
+    //}
+
+    if (read(newsockfd, buffer, 255) < 0) {
+      close(newsockfd);
+      continue;
+      // error("reading from socket");
+    }
     //printf("Got stuff %s\n", buffer);
     if (strncmp("GET / HTTP", buffer, 10) == 0){
-      if (write(newsockfd, webpage, webpagelen) < 0)
-        error("writing to socket");
+      if (write(newsockfd, webpage, webpagelen) < 0) {
+        close(newsockfd);
+        continue;
+        // error("writing to socket");
+      }
     }else{
-      if (write(newsockfd, errpage, errpagelen) < 0)
-        error("writing to socket");
+      if (write(newsockfd, errpage, errpagelen) < 0) {
+        close(newsockfd);
+        continue;
+        // error("writing to socket");
+      }
     }
-    sleep(1); // http://blog.csdn.net/CPP_CHEN/article/details/29864509
+    shutdown(newsockfd, SHUT_RDWR); // sleep(1); // http://blog.csdn.net/CPP_CHEN/article/details/29864509
     close(newsockfd);
   }
   close(sockfd);
-  return 0; 
+  return 0;
 }
